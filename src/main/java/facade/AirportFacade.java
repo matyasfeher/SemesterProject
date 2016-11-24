17cb653ca2747b272aacfdf958218a167d94e47a
matyasfeher <fehermatyas17@gmail.com>
2016-11-24 16:14:30 +0100

    Fetching airports

---
+++ src/main/java/facade/AirportFacade.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facade;
+
+import entity.Airport;
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author Acer
+ */
+public class AirportFacade {
+
+    private EntityManager getEntityManager() {
+        EntityManager em = Persistence.createEntityManagerFactory("pu").createEntityManager();
+        return em;
+    }
+
+    public void addAirports(Airport a) {
+        EntityManager em = getEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.persist(a);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+
+    }
+
+}
