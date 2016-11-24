d3eb46996f052b887c612232fbdbf25eaa817520
edipetres <edipetres@10.50.130.250>
2016-11-24 13:46:46 +0100

    prepare for takeoff

--- src/main/java/request/Flights.java
+++ src/main/java/request/Flights.java
@@ -10,49 +10,56 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import static javax.ws.rs.core.HttpHeaders.USER_AGENT;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.JSONObject;
 
 /**
  *
  * @author Nicolai
  */
 public class Flights {
-    
+
 //    public String getFlights(String depAirport, String depDate, int tickets){
 //        
 //    }
-    
     public static void main(String[] args) throws Exception {
         getFlightSite();
-        
+
     }
-    private static void getFlightSite() throws Exception{
-        
+
+    private static void getFlightSite() throws Exception {
+        String jsonResponse = null;
         try {
-        String url= "http://airline-plaul.rhcloud.com/api/flightinfo/cph/2017-01-06T08:00:00.000Z/2";
-        
-        URL obj = new URL(url);
-        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-        
-        con.setRequestMethod("GET");
-        
-        con.setRequestProperty("User-agent", USER_AGENT);
-        
-        int responseCode = con.getResponseCode();
-        System.out.println("\nSending 'GET' request to URL : " + url);
-        System.out.println("Response code : " + responseCode);
-        
-        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        StringBuilder response = new StringBuilder();
-        
-        while((inputLine = in.readLine()) != null){
-            response.append(inputLine + "\n");
-        }
-        in.close();
-        System.out.println(response.toString());
-        
+            String url = "http://airline-plaul.rhcloud.com/api/flightinfo/cph/2017-01-06T08:00:00.000Z/2";
+
+            URL obj = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+            con.setRequestMethod("GET");
+
+            con.setRequestProperty("User-agent", USER_AGENT);
+
+            int responseCode = con.getResponseCode();
+            System.out.println("\nSending 'GET' request to URL : " + url);
+            System.out.println("Response code : " + responseCode);
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+            
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine + "\n");
+            }
+            jsonResponse = response.toString();
+            in.close();
+            System.out.println(response.toString());
         } catch (Exception e) {
-            e.printStackTrace();}
-        
+            e.printStackTrace();
+        }
+
+        //Work with received JSON object
+        JSONParser parser = new JSONParser();
+        JSONObject jsonObject = (JSONObject) parser.parse(jsonResponse);
+        System.out.println("AIrline: "+jsonObject.get("airline"));
     }
 }
